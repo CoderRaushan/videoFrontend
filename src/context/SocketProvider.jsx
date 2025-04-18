4d425bb661a1b26daaaf923f066c3537cc1d6e76
CoderRaushan <raushankumar23082004@gmail.com>
2025-04-18 12:37:06 +0530

    first commit

--- src/context/SocketProvider.jsx
+++ src/context/SocketProvider.jsx
@@ -9,7 +9,7 @@ export const useSocket = () => {
 };
 
 export const SocketProvider = (props) => {
-  const socket = useMemo(() => io("localhost:8000"), []);
+  const socket = useMemo(() => io("https://videobackend-b5jl.onrender.com"), []);
 
   return (
     <SocketContext.Provider value={socket}>
